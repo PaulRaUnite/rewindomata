7e8e0ef4a3b74995466f74d390894f2822bcd34a
Pavlo Tokariev <16649084+PaulRaUnite@users.noreply.github.com>
2018-04-30 15:20:57 +0300

    Update examples, remove parallel search because of instability.

--- automaton/search_test.go
+++ automaton/search_test.go
@@ -1,21 +1,23 @@
 package automaton
 
 import (
-	"fmt"
 	"testing"
-
+	"fmt"
 	"github.com/PaulRaUnite/rewindomata/ast"
 )
 
-func TestAcceptor_Searches(t *testing.T) {
-	examples := map[string]map[bool][]string{
-		"ab+":    {true: {"ab"}, false: {"a", "b"}},
-		"ab|":    {true: {"a", "b"}, false: {"ab"}},
-		"ab+*":   {true: {"", "ab", "ab"}, false: {"ba", "bb"}},
-		"abc++":  {true: {"abc"}, false: {"ba", "bb", "ac"}},
-		"ab+c*|": {true: {"", "cc", "ab"}, false: {"ac", "g"}},
-	}
+var examples = map[string]map[bool][]string{
+	"ab+":    {true: {"ab"}, false: {"a", "b", ""}},
+	"ab|":    {true: {"a", "b"}, false: {"ab", "bc", "ac"}},
+	"ab+*":   {true: {"", "ab", "abab", "ababab"}, false: {"ba", "bb", "aa", "cc"}},
+	"abc++":  {true: {"abc"}, false: {"ba", "bb", "ac"}},
+	"ab+c*|": {true: {"", "cc", "ab"}, false: {"ac", "g"}},
+	"ab+c|":  {true: {"ab", "c"}, false: {"abc"}},
+	"a*":     {true: {"a", "aa", "aaa"}, false: {"b", "c", "abb"}},
+	"ab|*":   {true: {"aabb", "abba", "abaa"}, false: {"aaac", "bbbbbbbbbbad"}},
+}
 
+func TestAcceptor_Searches(t *testing.T) {
 	for regexp, resultCases := range examples {
 		tree, err := ast.Parse(regexp)
 		if err != nil {
@@ -32,10 +34,6 @@ func TestAcceptor_Searches(t *testing.T) {
 					t.Error(regexp, result, c)
 				}
 				if acc.FrontSearch(c) != result {
-					fmt.Println(acc)
-					t.Error(regexp, result, c)
-				}
-				if acc.AtomicParallelSearch(c, 3) != result {
 					fmt.Println(acc)
 					t.Error(regexp, result, c)
 				}
